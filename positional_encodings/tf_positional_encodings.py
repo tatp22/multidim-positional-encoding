9c2c21c4c51ad3706871edd67dd6c25a0a552f30
IRiViI <rckvnk@gmail.com>
2021-12-21 11:46:16 +0100

    Debugged undefined batch_size problem

--- positional_encodings/tf_positional_encodings.py
+++ positional_encodings/tf_positional_encodings.py
@@ -2,8 +2,9 @@
 import tensorflow as tf
 import numpy as np
 
-class TFPositionalEncoding2D(tf.keras.layers.Layer):
+import tensorflow as tf
 
+class TFPositionalEncoding2D(tf.keras.layers.Layer):
     def __init__(self, channels, dtype=tf.float32):
         """
         :param channels: The last dimension of the tensor you want to apply pos emb to.
@@ -11,7 +12,8 @@ class TFPositionalEncoding2D(tf.keras.layers.Layer):
         super(TFPositionalEncoding2D, self).__init__()
         self.channels = int(2 * np.ceil(channels/4))
         self.inv_freq = np.float32(1 / np.power(10000, np.arange(0, self.channels, 2) / np.float32(self.channels)))
-
+        
+    @tf.function
     def call(self, inputs):
         """
         :param tensor: A 4d tensor of size (batch_size, x, y, ch)
@@ -19,7 +21,8 @@ class TFPositionalEncoding2D(tf.keras.layers.Layer):
         """
         if len(inputs.shape)!=4:
             raise RuntimeError("The input tensor has to be 4d!")
-        batch_size, x, y, org_channels = inputs.shape
+        _, x, y, org_channels = inputs.shape
+        
         dtype = self.inv_freq.dtype
         
         pos_x = tf.range(x, dtype=dtype)
@@ -30,9 +33,7 @@ class TFPositionalEncoding2D(tf.keras.layers.Layer):
         
         emb_x = tf.expand_dims(tf.concat((tf.sin(sin_inp_x), tf.cos(sin_inp_x)), -1),1)
         emb_y = tf.expand_dims(tf.concat((tf.sin(sin_inp_y), tf.cos(sin_inp_y)), -1),0)
-
         emb_x = tf.tile(emb_x, (1,y,1))
         emb_y = tf.tile(emb_y, (x,1,1))
         emb = tf.concat((emb_x, emb_y),-1)
-
-        return tf.repeat(emb[None, :, :, :org_channels], batch_size, axis=0)
+        return tf.repeat(emb[None, :, :, :org_channels], tf.shape(inputs)[0], axis=0)
