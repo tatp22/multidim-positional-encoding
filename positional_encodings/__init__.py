4e485a780ee26c6b27b819733a7a4fd02b9a2a2d
Peter Tatkowski <peter.tatkowski@1plusx.com>
2021-12-31 13:01:43 +0100

    ch

--- positional_encodings/__init__.py
+++ positional_encodings/__init__.py
@@ -1,9 +1,5 @@
 from positional_encodings.positional_encodings import (
-    PositionalEncoding1D,
-    PositionalEncoding2D,
-    PositionalEncoding3D,
-    PositionalEncodingPermute1D,
-    PositionalEncodingPermute2D,
-    PositionalEncodingPermute3D,
-)
+    PositionalEncoding1D, PositionalEncoding2D, PositionalEncoding3D,
+    PositionalEncodingPermute1D, PositionalEncodingPermute2D,
+    PositionalEncodingPermute3D)
 from positional_encodings.tf_positional_encodings import TFPositionalEncoding2D
