77c30ceef355e5ef07756e02280e9e606e04b61c
Peter Tatkowski <tatp22@gmail.com>
2022-06-09 20:45:05 +0200

    update ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="positional_encodings",
-    version="5.0.0",
+    version="5.1.0",
     author="Peter Tatkowski",
     author_email="tatp22@gmail.com",
     description="1D, 2D, and 3D Sinusodal Positional Encodings in PyTorch",
