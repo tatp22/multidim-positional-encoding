57542dfb04d8f3b97775c3ee271a5b523907e046
Peter Sobot <psobot@spotify.com>
2024-01-04 10:48:05 -0500

    Make cached_penc a non-persistent buffer.

--- positional_encodings/torch_encodings.py
+++ positional_encodings/torch_encodings.py
@@ -22,7 +22,7 @@ class PositionalEncoding1D(nn.Module):
         self.channels = channels
         inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
         self.register_buffer("inv_freq", inv_freq)
-        self.register_buffer("cached_penc", None)
+        self.register_buffer("cached_penc", None, persistent=False)
 
     def forward(self, tensor):
         """
@@ -76,7 +76,7 @@ class PositionalEncoding2D(nn.Module):
         self.channels = channels
         inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
         self.register_buffer("inv_freq", inv_freq)
-        self.register_buffer("cached_penc", None)
+        self.register_buffer("cached_penc", None, persistent=False)
 
     def forward(self, tensor):
         """
@@ -140,7 +140,7 @@ class PositionalEncoding3D(nn.Module):
         self.channels = channels
         inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
         self.register_buffer("inv_freq", inv_freq)
-        self.register_buffer("cached_penc", None)
+        self.register_buffer("cached_penc", None, persistent=False)
 
     def forward(self, tensor):
         """
